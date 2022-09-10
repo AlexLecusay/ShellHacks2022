d5897d0174796a57650f6459301a570bcda339cd
Alex Lecusay <alex.lecusay@gmail.com>
2022-09-10 12:05:36 -0400

    Changes include adding Logo for the application and second activity

---
+++ StreamBill/app/src/main/java/com/example/streambill/DifferentStreamingServices.kt
@@ -0,0 +1,11 @@
+package com.example.streambill
+
+import android.app.Activity
+import android.os.Bundle
+
+class DifferentStreamingServices : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_streaming_services)
+    }
+}
--- StreamBill/app/src/main/java/com/example/streambill/MainActivity.kt
+++ StreamBill/app/src/main/java/com/example/streambill/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.streambill
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val mTestingButton : Button = findViewById(R.id.main_button_id)
+
+        mTestingButton.setOnClickListener {
+            startActivity(Intent(this, DifferentStreamingServices::class.java))
+        }
     }
 }
